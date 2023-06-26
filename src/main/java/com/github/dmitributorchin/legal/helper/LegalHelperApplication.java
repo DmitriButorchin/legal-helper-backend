6006a21b56968f88dba52d409d930d4d7129c265
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2023-06-26 23:57:29 +0300

    create project

---
+++ src/main/java/com/github/dmitributorchin/legal/helper/LegalHelperApplication.java
@@ -0,0 +1,13 @@
+package com.github.dmitributorchin.legal.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalHelperApplication.class, args);
+	}
+
+}
